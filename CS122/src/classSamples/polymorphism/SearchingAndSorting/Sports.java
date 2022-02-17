021d0f8090aacf2624c38132260261fb68cebbac
Justin10702 <98415768+Justin10702@users.noreply.github.com>
2022-02-17 14:11:27 -0500

    Class Lab 02/17/2022 Resubmit
    This is the resubmit of the class lab from 02/17/2022. My name is Justin Boudreau and I am in section 12:15-2:15 TR.

--- CS122/src/classSamples/polymorphism/SearchingAndSorting/Sports.java
+++ CS122/src/classSamples/polymorphism/SearchingAndSorting/Sports.java
@@ -14,22 +14,22 @@ public class Sports implements Comparable <Sports>{
 	// create getter methods for name and players
 	
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	public int getPlayers() {
-		return players;
+		return this.players;
 	}
 	
 	// create toString method 
 	public String toString() {
-		return "There are " + getPlayers() + " players. Name: " + getName();
+		return "There are " + this.players + " players. Name: " + this.name;
 	}
 
-	// override equals method 
+	// override equals method, comparing using name
 	
 	public boolean equals(Object o) {
-		return (name.equals(((Sports) o).getName()) && players == (((Sports) o).getPlayers()));
+		return (this.name.equals(((Sports) o).getName()));
 	}
 	
 	
@@ -38,7 +38,9 @@ public class Sports implements Comparable <Sports>{
 	@Override
 	public int compareTo(Sports o) {
 
-	return name.compareTo(o.getName());
+	return this.name.compareTo(o.getName());
 
+	
+	
 	}
 }

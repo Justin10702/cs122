84e763fee6fa3130251d75a8147fef35be172cdf
Justin10702 <98415768+Justin10702@users.noreply.github.com>
2022-03-10 12:49:17 -0500

    Class Lab 03/10/2022
    My name is Justin Boudreau, and I am in section 12:15-2:15 PM TR.

---
+++ CS122/src/classlab/week7/Division.java
@@ -0,0 +1,16 @@
+package classlab.week7;
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		int y = 0;
+		
+		System.out.println(x/y);
+		System.out.println("Hello");
+		
+		int[] z= new int[8];
+		System.out.println(z[10]);
+	}
+}
